49e689d470b868d453722798ef4383fc51b2f025
Félix Jullien <jullienfelix@gmail.com>
2023-09-13 17:26:33 +0200

    Fix compilation error

--- src/parsing/join.cpp
+++ src/parsing/join.cpp
@@ -3,7 +3,7 @@
 #define USAGE "JOIN <channel> (<password>)"
 #define ARGC_ERR "JOIN arg count error: " USAGE
 
-const std::string cmdJoin(const std::string &cmd, std::vector<std::string> args, Client &client, IrcServer &server) {
+const std::string cmdJoin(std::vector<std::string> args, Client &client, IrcServer &server) {
     int argc = args.size();
     if (argc == 0 || argc > 2)
        return ARGC_ERR;
@@ -16,11 +16,13 @@ const std::string cmdJoin(const std::string &cmd, std::vector<std::string> args,
     else if (channel->isInviteOnly && !channel->isUserOnInviteList(client))
         return "You need an invitation to join channel " + channelName +"\r\n";
     else if (channel->isRestricted)
+    {
         if( !pwProvided)
             return "Channel " + channelName + "requires a password"+"\r\n";
         std::string &pw = args[1];
         if (!channel->checkPassword(pw))
             return "Wrong password for Channel " + channelName +"\r\n";
+    }
     channel->addUser(client);
     return "You have join channel "  + channelName+"\r\n";
 }
