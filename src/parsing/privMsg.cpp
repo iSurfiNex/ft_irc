bd6bcf983e9556288017619f7a8ef3c4e2fbf353
Remi Sterin <banacraft@gmail.com>
2023-09-14 18:03:48 +0200

    remove space in error msg

--- src/parsing/privMsg.cpp
+++ src/parsing/privMsg.cpp
@@ -6,7 +6,7 @@
 /*   By: rsterin <rsterin@student.42angouleme.fr>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/13 16:58:52 by rsterin           #+#    #+#             */
-/*   Updated: 2023/09/14 15:58:28 by rsterin          ###   ########.fr       */
+/*   Updated: 2023/09/14 17:21:47 by rsterin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,14 +15,14 @@
 std::string cmdPrivMsg(std::vector<std::string> &args, Client &origin, IrcServer &server)
 {
 	if (args.size() != 2)
-		return ("Wrong number of arguments. Usage: PRIVMSG <channel/nickname> : <msg>.\r\n");
+		return ("Wrong number of arguments. Usage: PRIVMSG <channel/nickname> :<msg>.\r\n");
 
 	if (args[0][0] == '#')
 	{
 		Channel *channel;
 		channel = server.getChannelWithName(args[0]);
 		if (!channel || !channel->isUserInside(origin))
-			return ("Channel not found. Please enter a valid channel using: PRIVMSG <channel/nickname> : <msg>.\r\n");
+			return ("Channel not found. Please enter a valid channel using: PRIVMSG <channel/nickname> :<msg>.\r\n");
 
 		std::string messageToChannel = ":" + origin.nickname + " PRIVMSG " + channel->name + " :" + args[1];
 		std::cout << origin << " send: \"" << messageToChannel << "\" to " << channel->name << std::endl;
@@ -34,7 +34,7 @@ std::string cmdPrivMsg(std::vector<std::string> &args, Client &origin, IrcServer
 		Client *target;
 		target = server.getClientWithNickname(args[0]);
 		if (!target)
-			return ("User not found. Please enter a valid user using: PRIVMSG <channel/nickname> : <msg>.\r\n");
+			return ("User not found. Please enter a valid user using: PRIVMSG <channel/nickname> :<msg>.\r\n");
 
 		std::string messageToTarget = ":" + origin.nickname + " PRIVMSG " + target->nickname + " :" + args[1];
 		std::cout << origin << " send: \"" << messageToTarget << "\" to " << *target << std::endl;
