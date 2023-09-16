261ef844066c084569412602b2b8cc549ecd5125
Félix JULLIEN <jullienfelix@gmail.com>
2023-09-16 19:31:54 +0200

    topic fix

--- src/parsing/topic.cpp
+++ src/parsing/topic.cpp
@@ -20,8 +20,10 @@ void cmdTopic(strVec_t &args, Client &origin, IrcServer &server)
 	{
 		Channel *channel;
 		channel = server.getChannelWithName(args[0]);
-		if (!channel || !channel->isUserInside(origin))
+		if (!channel)
 			origin.msg(ERR_NOSUCHCHANNEL, args[0]);
+		else if (!channel->isUserInside(origin))
+			origin.msg(ERR_NOTONCHANNEL, args[0]);
 		else if (args.size() == 1)
 		{
 			if (channel->topic.empty())
@@ -31,8 +33,8 @@ void cmdTopic(strVec_t &args, Client &origin, IrcServer &server)
 		}
 		else if (channel->isMod(origin) || channel->isTopicChangeable)
 		{
-			channel->msg(MSG_NEWTOPIC, channel->topic);
 			channel->changeTopic(args[1]);
+			channel->msg(MSG_NEWTOPIC, channel->topic);
 		}
 		else
 			origin.msg(ERR_CHANOPRIVSNEEDED, channel->name);
