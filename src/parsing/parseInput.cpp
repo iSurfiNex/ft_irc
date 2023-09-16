88573e9ec9ebaf4067a0b6cd81ff3f04a2c916d3
Remi Sterin <banacraft@gmail.com>
2023-09-16 20:00:29 +0200

    pour felix

--- src/parsing/parseInput.cpp
+++ src/parsing/parseInput.cpp
@@ -6,7 +6,7 @@
 /*   By: rsterin <rsterin@student.42angouleme.fr>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/13 14:16:54 by rsterin           #+#    #+#             */
-/*   Updated: 2023/09/15 17:43:50 by rsterin          ###   ########.fr       */
+/*   Updated: 2023/09/16 20:00:20 by rsterin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,8 @@ void getCmdArgs(std::string buffer, std::string &cmd, strVec_t &args)
 		{
 			tmp.append(ptr);
 			tmp.erase(0, 1);
-			tmp += " ";
+			if (!tmp.empty())
+				tmp += " ";
 			two_points = true;
 		}
 		else if (two_points)
